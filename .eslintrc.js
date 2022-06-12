612c065e5626477fca12e24807163a1014cf570d
Michał Osman <michal.osman.997@gmail.com>
2022-06-12 13:46:28 +0200

    Update ESLint & Prettier configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,23 @@
 module.exports = {
   env: {
-    browser: true,
     node: true,
+    es2021: true,
   },
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: './tsconfig.eslint.json',
   },
   plugins: ['@typescript-eslint', 'prettier'],
   rules: {
-    '@typescript-eslint/ban-ts-comment': 'off',
-    // 'prettier/prettier': 'error',
+    'no-console': 0,
+    'consistent-return': 0,
+    '@typescript-eslint/no-shadow': 0,
+    '@typescript-eslint/no-throw-literal': 0,
+    'import/no-cycle': 0,
+    radix: 0,
   },
 }

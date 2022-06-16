965be717acb7d1ae1d49ce221bc5b323199f1f4e
Michał Osman <michal.osman.997@gmail.com>
2022-06-16 21:32:03 +0200

    Add ID validation

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import UserController from '../controllers/user.controller'
 import auth from '../middlewares/auth'
+import validateID from '../middlewares/validateID'
 
 const userRouter = Router()
 
@@ -10,11 +11,31 @@ userRouter.post('/logout', auth, UserController.logout)
 userRouter.post('/logout/all', auth, UserController.logoutAll)
 
 userRouter.get('/', auth, UserController.getAll)
-userRouter.get('/:id', auth, UserController.get)
+userRouter.get('/:id', auth, validateID, UserController.get)
 
-userRouter.patch('/:id/friend/request', auth, UserController.requestFriend)
-userRouter.patch('/:id/friend/accept', auth, UserController.acceptFriend)
-userRouter.patch('/:id/friend/reject', auth, UserController.rejectFriend)
-userRouter.patch('/:id/friend/remove', auth, UserController.removeFriend)
+userRouter.patch(
+  '/:id/friend/request',
+  auth,
+  validateID,
+  UserController.requestFriend
+)
+userRouter.patch(
+  '/:id/friend/accept',
+  auth,
+  validateID,
+  UserController.acceptFriend
+)
+userRouter.patch(
+  '/:id/friend/reject',
+  auth,
+  validateID,
+  UserController.rejectFriend
+)
+userRouter.patch(
+  '/:id/friend/remove',
+  auth,
+  validateID,
+  UserController.removeFriend
+)
 
 export default userRouter
